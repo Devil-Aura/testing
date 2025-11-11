3964598a9fe8c2df290996283e0cbbfc186b60b2
Devil-Aura <imh203356@gmail.com>
2025-11-11 23:11:40 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,14 @@
+# config.py
 API_ID = 22768311
 API_HASH = "702d8884f48b42e865425391432b3794"
 BOT_TOKEN = ""
 
-OWNER_ID = 7626220527  # your Telegram ID
-BACKUP_TIME = "23:30"  # 11:30 PM
-PORT = 8237  # web app port
+OWNER_ID = 7626220527   # your Telegram user id
+PORT = 8237
+
+# If you want MongoDB set this to your connection string, otherwise leave placeholder to use TinyDB
+MONGO_URI = "YOUR_MONGO_URI"
+MONGO_DBNAME = "linkprovider_db"
+MONGO_COLLECTION = "redirects"
+
+
